109a75ec354e4eb31cfe343d3ceb2872d610be0f
William Hettinger <bill.hettinger@gmail.com>
2018-02-25 15:30:19 -0500

    set up tableview with sample task objects

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by William Hettinger on 2/25/18.
+//  Copyright © 2018 William Hettinger. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tasks : [Task] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        var task = Task()
+        task = tasks[indexPath.row]
+        
+        
+        
+        if task.important {
+            cell.textLabel?.text = "❗️ \(task.name)"
+            cell.backgroundColor = UIColor.yellow
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task]{
+        let task1 = Task()
+        task1.name = "Walk the Dog"
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        
+        return [task1,task2,task3]
+    }
 }
 
